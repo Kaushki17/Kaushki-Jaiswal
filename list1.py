e0c514832e14c7e1b82283836b537bba290c5e97
Kaushki17 <kaushkijaiswal28@gmail.com>
2026-03-22 21:07:56 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,8 @@
+# Program by : Kaushki Jaiswal
+# Roll no: 35
+# List assignment : 1
+#Create a list of 5 friends names.Print the first and last name using positive and negative indexing.
+
+friends = ["Aman","Riya","Suhani","Priya","Rahul"]
+print("First friend(positive index): ",friends[0])
+print("Last friend(negative index): ",friends[-1])
